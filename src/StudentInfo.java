12589c83842f1a4978e6ba33988b124b4bf29149
Lenovo <Lenovo@Prathyangra>
2026-01-11 22:08:07 +0530

    Initial Commit-Java Program added

---
+++ src/StudentInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String args[]) {
+        System.out.println("Student Name: Prathyangra R");
+        System.out.println("Register Number: 23MIC0099");
+        System.out.println("Message: Jenkins–GitHub Integration Successful");
+    }
+}
