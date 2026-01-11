26f3d567a276ea6f501adfb54ae6c45199618df8
prathyangra <141577050+prathyangra@users.noreply.github.com>
2026-01-11 22:37:29 +0530

    Fix message formatting in StudentInfo.java

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -2,6 +2,6 @@ public class StudentInfo {
     public static void main(String args[]) {
         System.out.println("Student Name: Prathyangra R");
         System.out.println("Register Number: 23MIC0099");
-        System.out.println("Message: Jenkins–GitHub Integration Successful");
+        System.out.println("Message: Jenkins-GitHub Integration Successful");
     }
 }
