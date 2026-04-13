865759a5c9b516b9c815d5561bac46bafd42d5e5
prathyangra <141577050+prathyangra@users.noreply.github.com>
2026-04-13 14:12:30 +0530

    Refactor StudentInfo.java with new classes and Maven setup
    Refactor StudentInfo.java to include HomeController and BankService classes, along with Maven dependencies and Surefire plugin configuration.

--- src/StudentInfo.java
+++ src/StudentInfo.java
@@ -43,3 +43,79 @@ kubectl apply -f limited.yaml
 kubectl get pods
 kubectl describe pod limited-pod
 kubectl get all
+
+BUGZILLA-STS
+package com.example.demo,
+
+import org.springframework.web.bind.annotation.GetMapping.
+import org.springframework.web.bind.annotation.RestController:
+
+@RestController
+public class HomeController [
+@GetMapping("/")
+public String home() [
+return "Application Running Successfully":
+    }
+    }
+
+MAVEN
+<dependencies>
+<dependency>
+<groupId>org.junit.jupiter</groupId>
+<artifactId>junit-jupiter</artifactId>
+<version>5.10.0</version>
+<scope>test</scope>
+</dependency>
+</dependencies>
+
+Add Surefire plugin:
+
+<build>
+<plugins>
+<plugin>
+<groupId>org.apache.maven.plugins</groupId>
+<artifactId>maven-surefire-plugin</artifactId>
+<version>3.1.2</version>
+</plugin>
+</plugins>
+</build>
+
+src/main/java → com.bank → BankService.java
+
+package com.bank;
+public class BankService {
+public boolean transfer(double balance, double amount) {
+return balance >= amount && amount > 0;
+}
+}
+
+src/test/java → com.bank → BankServiceTest.java
+
+package com.bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+package sathya;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class maven {
+
+@Test
+public void validTransferTest() {
+maven service = new maven();
+assertTrue(service.transfer(1000, 500));
+}
+
+@Test
+public void invalidTransferTest() {
+maven service = new maven();
+assertFalse(service.transfer(200, 500));
+}
+
+public boolean transfer(int balance, int amount) {
+if (balance >= amount) {
+return true;
+}
+return false;
+}
+}
